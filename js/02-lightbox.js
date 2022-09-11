e6c40c6fc95f1f64d07d20f8a69169eb20fe1ada
ArthurS <arthurosh@gmail.com>
2022-09-11 09:24:21 +0200

    adds the execution of task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector(".gallery");
+const galleryEl = galleryMarkup(galleryItems);
+// console.log(galleryItems);
+
+container.insertAdjacentHTML("beforeend", galleryEl);
+
+function galleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+    <img
+      loading="lazy"
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`;
+    })
+    .join("");
+}
+const lightbox = new SimpleLightbox(".gallery a", {
+  /* options */
+  captionsData: "alt",
+  captionDelay: 250,
+});
